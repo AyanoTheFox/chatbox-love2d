39d948616f16791c6489ac8b45fdfe42ec8dd693
AyanoTheFox <ayanothefoxy@gmail.com>
2023-09-14 20:33:55 -0300

    first commit yay

--- conf.lua
+++ conf.lua
@@ -1,13 +1,16 @@
 function love.conf(t)
---^settings^--
+--screen
     t.window.fullscreen = true
-    t.window.width = 740
-    t.window.height = 460
---^game^--
-    t.window.title = "new project"
-    t.icon = nil
+    --window screen
+    t.window.title = "Icosaidrium"
+    t.window.icon = "assets/images/icosaidrium.png"
     t.author = "AyanoTheFox"
---^save^--
+--directory
     t.externalstorage = true
-    t.indentity = "FoxToolBoxDefault" 
+    t.identity = "Icosaidrium"
+--modules
+    t.modules.audio = false
+    t.modules.physics = false
+    t.modules.sound = false
+    t.modules.thread = false
 end
--- main.lua
+++ main.lua
@@ -1,24 +1,39 @@
-function love.load(_argc, _argv)
+function love.load()
     --^libs^--
     --#graphicals libs#--
     gs = require "libs/gamestate"
-    chatBox = require "libs/chatBox"
+    --#dataBank libs#--
+    json = require "libs/json"
+    lip = require "libs/lip"
+    --#others#--
+    suit = require "libs/suit"
+    timer = require "libs/timer"
+    require "libs/lep"
+    require "libs/addons"
     --^tables^--
     --#gamestate states#--
     states = {}
-    states.firstState = require "src/states/firstState"
+    states.loadscreen = require "src/states/loadscreen"
+    states.editor = require "src/states/editor"
+    states.manager = require "src/states/manager"
+    states.credits = require "src/states/credits"
     --^vars^--
-    font = love.graphics.setNewFont(12) --defaultFont
+    font = love.graphics.setNewFont("assets/fonts/phoenixBIOS.ttf", 12) --defaultFont
+    mobile = false --check if the user is a mobile user
     --^cmds^--
     love.graphics.setDefaultFilter("nearest", "nearest") --defaultfilter
-    gs.registerEvents({"update"; "textinput"; "textedited"; "keypressed"; "touchpressed"; "touchmoved"; "touchreleased"})
-    gs.switch(states.firstState)
+    gs.registerEvents({"update"; "textinput"; "textedited"; "keypressed"; "touchpressed"; "touchmoved"; "touchreleased"; "mousepressed"; "mousemoved"}) --events to register
+    gs.switch(states.loadscreen)
+    if love.system.getOS() == 'iOS' or love.system.getOS() == 'Android' then mobile = true end
 end
 
-function love.draw() gs.current():draw() end
+function love.draw()
+    gs.current():draw()
+    suit.draw()
+end
 
 function love.textedited(_t, _s, _l) suit.textedited(_t, _s, _l) end
 
 function love.keypressed(_k) suit.keypressed(_k) end
 
-function love.textinput(_t) suit.keypressed(_t) end
+function love.textinput(_t) suit.textinput(_t) end
